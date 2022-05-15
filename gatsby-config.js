1d6749d85790add02ebc37dd837b2abe52c92f1c
HYEON <helloworld.hellohyeon@gmail.com>
2022-05-15 20:43:50 +0900

    내용 수정

--- gatsby-config.js
+++ gatsby-config.js
@@ -65,7 +65,9 @@ module.exports = {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
-          `gatsby-remark-smarttypo`,
+          {
+            resolve: `gatsby-remark-smartypants`,
+          },
           {
             resolve: `gatsby-remark-images`,
             options: {
